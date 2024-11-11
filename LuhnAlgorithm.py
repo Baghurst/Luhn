e536e739cb57f1b6ee5301fdba858e309b8ad38b
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-11-11 13:41:26 +0000

    Add files via upload

---
+++ LuhnAlgorithm.py
@@ -0,0 +1,41 @@
+
+#accountnum = 79927398713
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 1: "))
+n3 = int(input("Enter number 1: "))
+n4 = int(input("Enter number 1: "))
+n5 = int(input(""))
+n6 = int(input(""))
+n7 = int(input(""))
+n8 = int(input(""))
+n9 = int(input(""))
+n10 = int(input(""))
+n11 = int(input(""))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
