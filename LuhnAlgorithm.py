6fac08479fef1eda674cf8ee0557fa4f07c6fd46
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-11-13 12:01:40 +0000

    Update LuhnAlgorithm.py

--- LuhnAlgorithm.py
+++ LuhnAlgorithm.py
@@ -1,24 +1,34 @@
 
 #accountnum = 79927398713
-user_number = (input("Enter a number: "))
+user_account = int(input("Enter a number: "))
 running_total = 0
-count = 0    
+counter = 0    
 odd = 0
 even = 0
-while account > 0:
-    digit = account %10
-    account = account // 10
-    counter = counter + 1
-    if count % 2 == 0:
-        digit2=digit*2
-        print (digit2)
-        print ()
-        if digit2>9:
-            while digit2>0:
-                tmp = digit2 % 10
-                print (tmp)
-                digit2=digit2 // 10
-                even = even + tmp
+while user_account > 0:
+    digit = user_account %10 #Gives the remainder of the number which is the very last number
+    user_account = user_account // 10 #Double divides the number by 10 taking away the last number, and repeating it with every number after. 
+    counter = counter + 1 #Counts all the numbers starting from the first one
+    if counter % 2 == 0: #Finds the even number
+        digit2=digit*2 #Multiples by 2 all even digits
+        print (digit2) #Prints the result of the even numbers multiplied by 2
+        print () #Gives a blank space
+        if digit2>9: #Checks if the even numbers multiplied by 2 are bigger than 9
+            while digit2>0: #Starts a loop if the multiplied numbers are bigger than 9
+                tmp = digit2 % 10 #Finds the remainder of the multiplied number
+                print (tmp) #Prints the remainder
+                digit2=digit2 // 10 #Changes the value of digit2 by double dividing it by 10
+                even = even + tmp #Changes the value of even by adding it with tmp (the value of the remainder of the multiplied number)
+                print(even)
+        else:
+              even = even + digit
+    else:
+          odd = odd + digit
+
+print(odd + even)
+
+
+
 
 
 
