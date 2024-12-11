99bfd56ec13b8ae12eee74656c49ca5deb574e9d
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-12-11 11:59:17 +0000

    Update LuhnAlgorithm.py

--- LuhnAlgorithm.py
+++ LuhnAlgorithm.py
@@ -14,32 +14,22 @@ while user_account > 0:
         print (digit2) #Prints the result of the even numbers multiplied by 2
         print () #Gives a blank space
         if digit2>9: #Checks if the even numbers multiplied by 2 are bigger than 9
-            while digit2>0: #Starts a loop if the multiplied numbers are bigger than 9
+            while digit2>1: #Starts a loop if the multiplied numbers are bigger than 9
+                print('digit2 is: ', digit2)
                 tmp = digit2 % 10 #Finds the remainder of the multiplied number
-                print (tmp) #Prints the remainder
+                print ('first digit from double digit number: ',tmp) #Prints the remainder
+                even = even + tmp
                 digit2=digit2 // 10 #Changes the value of digit2 by double dividing it by 10
-                even = even + tmp #Changes the value of even by adding it with tmp (the value of the remainder of the multiplied number)
-                print(even)
+                even = even + digit2 #Changes the value of even by adding it with tmp (the value of the remainder of the multiplied number)
+                print('last digiti from double digit number: ', digit2)
+                print('Even is: ',even)
         else:
-              even = even + digit
+            even = even + digit2
+            print('Even is: ', even)
     else:
-          odd = odd + digit
+        odd = odd + digit
 
 print(odd + even)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
